481cb63d574927450c0e13a43903e64deee2db5d
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-03-25 00:45:33 +0700

    Build category functionality in home module

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\HomeService;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    private $homeService;
+
+    public function __construct() {
+        $this->homeService = new HomeService();
+    }
+
     public function index() {
         $data['pageTitle'] = 'Home page';
-        return view('pages.home.home-page', [
-            'data' => $data,
-        ]);
+        $data['categoryTrees'] = $this->homeService->getCategoryTrees();
+
+        return view('pages.home.home-page', ['data' => $data]);
     }
 }
---
+++ app/Services/HomeService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Log;
+
+class HomeService
+{
+    public function getCategoryTrees()
+    {
+        $categoryTrees = [];
+        $categories = Category::where([
+            'delete_flag' => false,
+            'parent_id' => null,
+        ])->get();
+
+        foreach ($categories as $category) {
+            array_push($categoryTrees, $this->getCategoryTree($category));
+        }
+
+        return $categoryTrees;
+    }
+
+    private function getCategoryTree($category)
+    {
+        $categoryTree = $category->getAttributes();
+        $categoryTree['children'] = [];
+
+        $categoryChildren = Category::where([
+            'delete_flag' => false,
+            'parent_id' => $category->id,
+        ])->get();
+        foreach ($categoryChildren as $categoryChild) {
+            array_push($categoryTree['children'], $this->getCategoryTree($categoryChild));
+        }
+
+        return $categoryTree;
+    }
+}
