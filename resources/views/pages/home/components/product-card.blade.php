3b9488a14cad4546152521f00eed33fcfb2eb5bb
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-04-02 07:31:33 +0700

    Fix bug UI

--- resources/views/pages/home/components/product-card.blade.php
+++ resources/views/pages/home/components/product-card.blade.php
@@ -1,20 +1,22 @@
 <div class="product-card">
-    <div class="product-card__image-wrapper">
-        <img src="{{ asset('storage/' . $product->main_image_path) }}" alt="">
-    </div>
+    <a href="#" class="product-card__link">
+        <div class="product-card__image-wrapper">
+            <img src="{{ asset('storage/' . $product->main_image_path) }}" alt="">
+        </div>
 
-    <div class="product-card__name-wrapper">
-        <h3>{{ $product->name }}</h3>
-    </div>
+        <div class="product-card__name-wrapper">
+            <h3>{{ $product->name }}</h3>
+        </div>
 
-    <div class="product-card__price-wrapper">
-        @if ($product->discount_percent === 0)
-            <span>{{ '$' . number_format($product->price) }}</span>
-        @else
-            <span>{{ '$' . number_format($product->price * (1 - $product->discount_percent / 100)) }}</span>
-            <span><del>{{ '$' . number_format($product->price) }}</del></span>
-        @endif
-    </div>
+        <div class="product-card__price-wrapper">
+            @if ($product->discount_percent === 0)
+                <span>{{ '$' . number_format($product->price) }}</span>
+            @else
+                <span>{{ '$' . number_format($product->price * (1 - $product->discount_percent / 100)) }}</span>
+                <span><del>{{ '$' . number_format($product->price) }}</del></span>
+            @endif
+        </div>
+    </a>
 
     <div class="product-card__action-wrapper">
         <a href="#favorite">
