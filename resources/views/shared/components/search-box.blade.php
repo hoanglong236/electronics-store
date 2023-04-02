9595287b49c0c1f2dab78da333a08d5f3ad0a40d
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-04-02 11:23:17 +0700

    Fix bug UI

---
+++ resources/views/shared/components/search-box.blade.php
@@ -0,0 +1,8 @@
+<!-- search box -->
+<div class="header-search-box">
+    <form action="">
+        <input type="text" placeholder="Search here ex. 'man' ">
+        <button type="submit"><span class="fa fa-search"></span></button>
+    </form>
+</div>
+<!-- / search box -->
